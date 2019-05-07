9c36f1ae8ecb468baa649e0cb54e362c0e594218
jossef <jossef12@gmail.com>
2019-05-07 06:53:29 +0300

    Ability to see codepoints from Demo Page close #29

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -13,26 +13,22 @@
             <div class="icons-gallery-header-link">
                 <a target="_blank" href="https://github.com/jossef/material-design-icons-iconfont">View Project in GitHub</a>
             </div>
-        </div>
-        <div class="icons-gallery-content">
-            <div class="icons-gallery-content-floating-search">
-                <div class="icons-gallery-content-floating-search-input">
+            <div class="icons-gallery-header-floating-search">
+                <div class="icons-gallery-header-floating-search-input">
                     <input placeholder="Filter Icons ..." type="text" v-model="filter">
                 </div>
             </div>
+        </div>
+        <div class="icons-gallery-content">
+
 
             <div class="icons-gallery-content-gallery">
-                <div class="icons-gallery-content-gallery-category" v-for="category in filteredIconCategories" v-bind:key="category.name">
-
-
-                    <div class="icons-gallery-content-gallery-category-header">
-                        {{formatName(category.name)}} Icons
-                    </div>
+                <div class="icons-gallery-content-gallery-category">
 
                     <div class="icons-gallery-content-gallery-category-icons">
                         <div class="icons-gallery-content-gallery-category-icons-wrapper"
                              v-bind:class="{'icons-gallery-content-gallery-category-icons-wrapper-active': isIconSelected(icon.id), 'icons-gallery-content-gallery-category-icons-wrapper-disabled': isAnyIconSelected() && !isIconSelected(icon.id), }"
-                             v-for="icon in category.icons" v-bind:key="icon.id"
+                             v-for="icon in filteredIcons" v-bind:key="icon.id"
                              v-on:click="toggleSelectIcon(icon.id)"
                         >
                             <div class="icons-gallery-content-gallery-category-icons-wrapper-icon">
@@ -48,7 +44,7 @@
                 </div>
 
                 <div class="icons-gallery-content-gallery-no-filter-results"
-                     v-if="!filteredIconCategories.length">
+                     v-if="!filteredIcons.length">
 
                     <div class="icons-gallery-content-gallery-no-filter-results-message">
                         <div class="icons-gallery-content-gallery-no-filter-results-message-icon">🤣</div>
@@ -71,7 +67,12 @@
                 </div>
 
 
-                <div v-if="isAnyIconSelected()" class="icons-gallery-content-info-header icons-gallery-content-info-header-padding">selected icons ({{selectedIconsCount}})</div>
+                <div v-if="isAnyIconSelected()" class="icons-gallery-content-info-header icons-gallery-content-info-header-padding">
+                    <div class="icons-gallery-content-info-header-text">selected icons ({{selectedIconsCount}})</div>
+                    <div class="icons-gallery-content-info-header-button" v-bind:class="{'icons-gallery-content-info-header-button-active':showCodepoints}" @click="showCodepoints = !showCodepoints"><i class="material-icons" v-bind:class="showCodepoints ? 'check_box' : 'check_box_outline_blank'"></i>&nbsp; codepoints</div>
+                    <div class="icons-gallery-content-info-header-button" v-bind:class="{'icons-gallery-content-info-header-button-active':showClasses}" @click="showClasses = !showClasses"><i class="material-icons " v-bind:class="showClasses ? 'check_box' : 'check_box_outline_blank'"></i>&nbsp; css classes</div>
+                    <div class="icons-gallery-content-info-header-button icons-gallery-content-info-header-button-active" @click="clear()"><i class="material-icons clear"></i>&nbsp;clear</div>
+                </div>
                 <div class="icons-gallery-content-info-selected-icons">
                     <div class="icons-gallery-content-info-selected-icons-item"
                          v-for="(_, iconId) in selectedIcons" v-bind:key="iconId">
@@ -81,7 +82,11 @@
 
                         <div class="icons-gallery-content-info-selected-icons-item-info">
                             <div class="icons-gallery-content-info-selected-icons-item-info-name">{{iconId}}</div>
-                            <div class="icons-gallery-content-info-selected-icons-item-info-embed">&#x3C;i class=&#x22;material-icons&#x22;&#x3E;{{iconId}}&#x3C;/i&#x3E;</div>
+                            <div class="icons-gallery-content-info-selected-icons-item-info-embed" v-if="showClasses">&#x3C;i class=&#x22;material-icons {{normalizeCssClassName(iconId)}}&#x22;&#x3E;&#x3C;/i&#x3E;</div>
+                            <div class="icons-gallery-content-info-selected-icons-item-info-embed" v-else>&#x3C;i class=&#x22;material-icons&#x22;&#x3E;{{iconId}}&#x3C;/i&#x3E;</div>
+                            <div v-if="showCodepoints">codepoint:</div>
+                            <div class="icons-gallery-content-info-selected-icons-item-info-embed" v-if="showCodepoints">{{iconsMap[iconId].toLowerCase()}}</div>
+                            <div class="icons-gallery-content-info-selected-icons-item-info-embed" v-if="showCodepoints">let character = "\u{{iconsMap[iconId].toLowerCase()}}";</div>
 
                         </div>
                     </div>
@@ -102,9 +107,9 @@
 
 
 <script>
-    import data from "../data";
     import Vue from "vue";
     import logo from "../components/logo";
+    import iconsMap from "../assets/fonts/MaterialIcons-Regular.json";
 
     let vm;
 
@@ -116,10 +121,23 @@
             title: "Material Design Icons DX"
         },
         data() {
+            let icons = Object.keys(iconsMap);
+            icons.sort();
+
+            icons = icons.map((icon) => {
+                return {
+                    "id": icon,
+                    "codepoint": iconsMap[icon]
+                }
+            });
+
             return {
-                iconCategories: data.categories,
+                icons: icons,
+                iconsMap: iconsMap,
                 selectedIcons: {},
                 selectedIconsCount: 0,
+                showCodepoints: false,
+                showClasses: false,
                 filter: ""
             };
         },
@@ -127,21 +145,12 @@
             vm = this;
         },
         computed: {
-            filteredIconCategories() {
+            filteredIcons() {
                 if (!vm.filter) {
-                    return vm.iconCategories;
+                    return vm.icons;
                 }
 
-                let iconCategories = vm.iconCategories
-                    .map(iconCategory => {
-                        return {
-                            name: iconCategory.name,
-                            icons: iconCategory.icons.filter(icon => vm.isNotFiltered(icon.id))
-                        };
-                    })
-                    .filter(iconCategory => iconCategory.icons.length);
-
-                return iconCategories;
+                return vm.icons.filter(icon => vm.isNotFiltered(icon.id));
             }
         },
 
@@ -150,8 +159,15 @@
                 name = name.replace(/_/, " ");
                 return name.charAt(0).toUpperCase() + name.substr(1).toLowerCase();
             },
+            normalizeCssClassName(name){
+              if (/^\d.*/.test(name)){
+                  return `_${name}`;
+              }
+
+              return name;
+            },
             isNotFiltered(iconId) {
-                return vm.filter ? iconId.indexOf(vm.filter) >= 0 : true;
+                return vm.filter ? iconId.indexOf(vm.filter.trim()) >= 0 : true;
             },
             toggleSelectIcon(iconId) {
                 let value = !vm.selectedIcons[iconId];
@@ -168,6 +184,10 @@
             },
             isAnyIconSelected() {
                 return vm.selectedIconsCount;
+            },
+            clear(){
+              vm.selectedIcons = {};
+              vm.selectedIconsCount=0;
             }
         }
     };
@@ -184,6 +204,28 @@
     $color-white: #fff;
     $color-disabled: rgba(#000, 0.5);
     $color-green: #388e3c;
+
+    @mixin scrollbars($size, $foreground-color, $background-color: mix($foreground-color, white,  50%)) {
+        // For Google Chrome
+        ::-webkit-scrollbar {
+            width: $size;
+            height: $size;
+        }
+
+        ::-webkit-scrollbar-thumb {
+            background: $foreground-color;
+        }
+
+        ::-webkit-scrollbar-track {
+            background: $background-color;
+        }
+
+        // For Internet Explorer
+        body {
+            scrollbar-face-color: $foreground-color;
+            scrollbar-track-color: $background-color;
+        }
+    }
 
     @mixin card() {
         background: #fff;
@@ -228,6 +270,10 @@
         input {
             @include card();
 
+            box-shadow: 0px 4px 20px 0px rgba(251, 255, 0, 0.45),
+            0 1px 3px rgba(0, 0, 0, 0.12),
+            0 1px 2px rgba(0, 0, 0, 0.24);
+
             box-sizing: border-box;
             width: 100%;
 
@@ -242,9 +288,12 @@
             &:focus {
                 outline-width: 0;
 
-                box-shadow: rgb(229, 229, 229) 0px -1px 0px 0px,
+                box-shadow: 0px 3px 20px 0px rgba(255, 255, 255, 0.64),
+                rgb(229, 229, 229) 0px -1px 0px 0px,
                 rgba(0, 0, 0, 0.117647) 0px 0px 2px 0px,
                 rgba(0, 0, 0, 0.239216) 0px 4px 10px 0px;
+                transform: scale(1.2, 1.2);
+
             }
 
             &:focus,
@@ -292,6 +341,7 @@
             flex-direction: row;
             padding: 0 $padding;
             align-items: center;
+            position: relative;
 
             background: $background-color;
             color: #fff;
@@ -309,6 +359,25 @@
                 margin-left: $padding;
                 @include defuse-links(#fff);
             }
+
+            &-floating-search {
+                position: absolute;
+                $input-height: 30px;
+                width: 300px;
+                height: $input-height;
+                top: $header-height - $input-height/2;
+                right: $info-menu-width + $padding;
+                z-index: 2;
+                &-input {
+                    @include input($input-height);
+
+                    input {
+                        border-radius: 20px;
+
+                    }
+                }
+            }
+
         }
 
         &-content {
@@ -318,20 +387,9 @@
             overflow: hidden;
             flex-direction: row;
 
-            background-image: linear-gradient(to right, #fff, #eee);
-
-            &-floating-search {
-                position: absolute;
-                $input-height: 30px;
-                width: 300px;
-                height: $input-height;
-                top: 0;
-                right: $info-menu-width + $padding;
-                z-index: 2;
-                &-input {
-                    @include input($input-height);
-                }
-            }
+            //background-image: linear-gradient(to right, #fff, #eee);
+            background-color: #fff;
+            @include scrollbars(3px, #777, transparent);
 
             &-gallery {
                 flex-grow: 999;
@@ -342,12 +400,12 @@
                     display: flex;
                     flex-direction: column;
                     padding: $padding/2 $padding;
-
-                    box-shadow: 0 1px 0 0 #ccc;
+                    margin-top: $padding;
 
                     &-header {
                         font-size: 16px;
                         margin-bottom: $padding/2;
+
                     }
 
                     &-icons {
@@ -458,13 +516,48 @@
                 box-sizing: border-box;
                 overflow-y: auto;
                 padding: 10px;
-                background: #eee;
+                //background: #eee;
+                background-image: linear-gradient(to right, #fff, #eee);
 
                 &-header {
                     text-transform: uppercase;
                     font-size: 12px;
                     font-weight: 700;
                     margin-bottom: 4px;
+
+                    display: flex;
+                    flex-direction: row;
+                    align-items: center;
+
+                    &-text{
+                        flex-grow: 999;
+                    }
+
+                    &-button{
+                        user-select: none;
+                        cursor: pointer;
+
+                        padding: 4px 5px;
+                        border-radius: 8px;
+                        display: flex;
+                        align-items: center;
+                        margin-left: 6px;
+                        box-shadow: 0 0 0 1px #bdbdbd;
+                        opacity: 0.8;
+
+                        &:hover{
+                            opacity: 1;
+                        }
+
+                        i{
+                            font-size: 14px;
+                        }
+
+                        &-active{
+                            background: #eeed9e;
+
+                        }
+                    }
 
                     &-padding {
                         margin-top: $padding;
@@ -476,6 +569,7 @@
                     &-item {
                         @include card();
 
+                        margin-bottom: 4px;
                         display: flex;
                         flex-direction: row;
 

6d285b0b153d086c9da90522e812be3c9bd549c5
Manny Martins <mmartins@vntsc.dot.gov>
2019-08-12 09:13:21 -0400

    Latest icons

--- build/build.js
+++ build/build.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const mkdirp = require('mkdirp');
-const {promisify} = require('util');
+const { promisify } = require('util');
 const sass = require('node-sass');
 
 const request = require('request');
@@ -30,178 +30,172 @@ Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.
 Mozilla/5.0 (Linux; U; Android 4.4.2; zh-cn; GT-I9500 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko)Version/4.0 MQQBrowser/5.0 QQ-URL-Manager Mobile Safari/537.36
 Mozilla/5.0 (Android 8.0.0; Tablet; rv:57.0) Gecko/57.0 Firefox/57.0
 Mozilla/5.0 (Android 8.1.0; Mobile; rv:61.0) Gecko/61.0 Firefox/61.0
-`.split('\n').filter(x => !!x).map(x => x.trim());
-
+`
+  .split('\n')
+  .filter((x) => !!x)
+  .map((x) => x.trim());
 
 async function readFontLigatures(fontFilePath) {
+  let font = await promisify(fontkit.open)(fontFilePath);
+  let lookupList = font.GSUB.lookupList.toArray();
+  let lookupListIndexes = font.GSUB.featureList[0].feature.lookupListIndexes;
 
-    let font = await promisify(fontkit.open)(fontFilePath);
-    let lookupList = font.GSUB.lookupList.toArray();
-    let lookupListIndexes = font.GSUB.featureList[0].feature.lookupListIndexes;
+  let result = {};
 
-    let result = {};
+  lookupListIndexes.forEach((index) => {
+    let subTable = lookupList[index].subTables[0];
 
-    lookupListIndexes.forEach(index => {
-        let subTable = lookupList[index].subTables[0];
-
-        let leadingCharacters = [];
-        subTable.coverage.rangeRecords.forEach((coverage) => {
-            for (let i = coverage.start; i <= coverage.end; i++) {
-                let character = font.stringsForGlyph(i)[0];
-                leadingCharacters.push(character);
-            }
-        });
-
-        let ligatureSets = subTable.ligatureSets.toArray();
-
-        ligatureSets.forEach((ligatureSet, ligatureSetIndex) => {
-
-            let leadingCharacter = leadingCharacters[ligatureSetIndex];
-
-            ligatureSet.forEach(ligature => {
-                let character = font.stringsForGlyph(ligature.glyph)[0];
-                let characterCode = character.charCodeAt(0).toString(16).toUpperCase();
-
-                let ligatureText = ligature
-                    .components
-                    .map(x => font.stringsForGlyph(x)[0])
-                    .join('');
-
-                ligatureText = leadingCharacter + ligatureText;
-                result[ligatureText] = characterCode;
-            });
-        });
+    let leadingCharacters = [];
+    subTable.coverage.rangeRecords.forEach((coverage) => {
+      for (let i = coverage.start; i <= coverage.end; i++) {
+        let character = font.stringsForGlyph(i)[0];
+        leadingCharacters.push(character);
+      }
     });
 
-    return result;
+    let ligatureSets = subTable.ligatureSets.toArray();
+
+    ligatureSets.forEach((ligatureSet, ligatureSetIndex) => {
+      let leadingCharacter = leadingCharacters[ligatureSetIndex];
+
+      ligatureSet.forEach((ligature) => {
+        let character = font.stringsForGlyph(ligature.glyph)[0];
+        let characterCode = character
+          .charCodeAt(0)
+          .toString(16)
+          .toUpperCase();
+
+        let ligatureText = ligature.components.map((x) => font.stringsForGlyph(x)[0]).join('');
+
+        ligatureText = leadingCharacter + ligatureText;
+        result[ligatureText] = characterCode;
+      });
+    });
+  });
+
+  return result;
 }
 
 function downloadFile(url, outputFilePath) {
-    return new Promise((resolve, reject) => {
-        let file = fs.createWriteStream(outputFilePath);
-        let r = request.get(url);
+  return new Promise((resolve, reject) => {
+    let file = fs.createWriteStream(outputFilePath);
+    let r = request.get(url);
 
-        let onError = (error) => {
-            fs.unlink(outputFilePath, () => {
-                reject((error && error.message) || error);
-            });
-        };
+    let onError = (error) => {
+      fs.unlink(outputFilePath, () => {
+        reject((error && error.message) || error);
+      });
+    };
 
-        r.on('response', (response) => {
-            let hasError = (400 <= response.statusCode) && (response.statusCode < 600);
-            if (hasError) {
-                return onError(`Invalid response status code ${response.statusCode}`);
-            }
+    r.on('response', (response) => {
+      let hasError = 400 <= response.statusCode && response.statusCode < 600;
+      if (hasError) {
+        return onError(`Invalid response status code ${response.statusCode}`);
+      }
 
-            r.pipe(file);
-        });
-
-        file.on('finish', () => file.close(resolve));
-        r.on('error', onError);
-        file.on('error', onError);
-
+      r.pipe(file);
     });
-}
 
+    file.on('finish', () => file.close(resolve));
+    r.on('error', onError);
+    file.on('error', onError);
+  });
+}
 
 const outputDirPath = path.resolve(__dirname, '..', 'dist');
 const outputFontsDirPath = path.resolve(outputDirPath, 'fonts');
 const srcDirPath = path.resolve(__dirname, '..', 'src');
 
-async function writeVariablesSCSSFile(outputFilePath, fontLigatures){
-    let keys = Object.keys(fontLigatures);
-    keys.sort();
-    let lines = keys.map(key => `\t"${key}": ${fontLigatures[key].toLowerCase()},`);
+async function writeVariablesSCSSFile(outputFilePath, fontLigatures) {
+  let keys = Object.keys(fontLigatures);
+  keys.sort();
+  let lines = keys.map((key) => `\t"${key}": ${fontLigatures[key].toLowerCase()},`);
 
-    let content = `$material-icons-codepoints: () !default;
+  let content = `$material-icons-codepoints: () !default;
 $material-icons-codepoints: map-merge((
 ${lines.join('\n')}
 ), $material-icons-codepoints);`;
 
-    await promisify(fs.writeFile)(outputFilePath, content);
+  await promisify(fs.writeFile)(outputFilePath, content);
 }
 
 async function buildScss(sourceScssFilePath, outputCssFilePath) {
+  console.log(`generating css file "${outputCssFilePath}" ...`);
+  let content = await promisify(sass.render)({
+    file: sourceScssFilePath,
+  });
 
-    console.log(`generating css file "${outputCssFilePath}" ...`);
-    let content = await promisify(sass.render)({
-        file: sourceScssFilePath,
-    });
-
-    await promisify(fs.writeFile)(outputCssFilePath, content.css);
+  await promisify(fs.writeFile)(outputCssFilePath, content.css);
 }
 
 async function main() {
+  let sourceScssFilePath = path.resolve(srcDirPath, 'material-design-icons.scss');
+  let outputCssFilePath = path.resolve(outputDirPath, 'material-design-icons.css');
 
+  await buildScss(sourceScssFilePath, outputCssFilePath);
+  //return;
 
-    let sourceScssFilePath = path.resolve(srcDirPath, 'material-design-icons.scss');
-    let outputCssFilePath = path.resolve(outputDirPath, 'material-design-icons.css');
+  if (!(await promisify(fs.exists)(outputFontsDirPath))) {
+    await promisify(mkdirp)(outputFontsDirPath);
+  }
 
-    await buildScss(sourceScssFilePath, outputCssFilePath);
-    return;
+  let url = 'https://fonts.googleapis.com/icon?family=Material+Icons';
+  let fontName = 'MaterialIcons-Regular';
 
-    if (!await promisify(fs.exists)(outputFontsDirPath)) {
-        await promisify(mkdirp)(outputFontsDirPath);
-    }
+  let fontsUrls = {};
+  await Promise.all(
+    userAgents.map(async (userAgent) => {
+      let options = {
+        url: url,
+        headers: {
+          'User-Agent': userAgent,
+        },
+      };
 
-    let url = 'https://fonts.googleapis.com/icon?family=Material+Icons';
-    let fontName = 'MaterialIcons-Regular';
+      let content = await rp.get(options);
 
-    let fontsUrls = {};
-    await Promise.all(userAgents.map(async userAgent => {
-        let options = {
-            url: url,
-            headers: {
-                'User-Agent': userAgent
-            }
-        };
+      let matches = /url\((.*?)\)/g.exec(content);
+      let fontUrl = matches[1];
+      let extension = fontUrl.substr(fontUrl.lastIndexOf('.') + 1);
 
-        let content = await rp.get(options);
+      fontsUrls[extension] = fontUrl;
+    })
+  );
 
-        let matches = /url\((.*?)\)/g.exec(content);
-        let fontUrl = matches[1];
-        let extension = fontUrl.substr(fontUrl.lastIndexOf('.') + 1);
+  await Promise.all(
+    Object.keys(fontsUrls).map(async (extension) => {
+      let url = fontsUrls[extension];
 
-        fontsUrls[extension] = fontUrl;
+      let outputFilePath = path.resolve(outputFontsDirPath, `${fontName}.${extension}`);
 
-    }));
+      console.log(`downloading "${outputFilePath}" ...`);
+      await downloadFile(url, outputFilePath);
 
-    await Promise.all(Object.keys(fontsUrls).map(async extension => {
-        let url = fontsUrls[extension];
+      if (extension === 'ttf') {
+        let fontLigatures = await readFontLigatures(outputFilePath);
+        let outputLigaturesJsonFilePath = path.resolve(outputFontsDirPath, `${fontName}.json`);
 
-        let outputFilePath = path.resolve(outputFontsDirPath, `${fontName}.${extension}`);
+        console.log(`extracted mapping to "${outputLigaturesJsonFilePath}" ...`);
+        await promisify(fs.writeFile)(outputLigaturesJsonFilePath, JSON.stringify(fontLigatures, null, 4));
 
-        console.log(`downloading "${outputFilePath}" ...`);
-        await downloadFile(url, outputFilePath);
+        let outputVariablesScssFilePath = path.resolve(srcDirPath, '_variables.scss');
 
-        if (extension === 'ttf') {
-            let fontLigatures = await readFontLigatures(outputFilePath);
-            let outputLigaturesJsonFilePath = path.resolve(outputFontsDirPath, `${fontName}.json`);
+        console.log(`generating codepoints to "${outputVariablesScssFilePath}" ...`);
+        await writeVariablesSCSSFile(outputVariablesScssFilePath, fontLigatures);
 
-            console.log(`extracted mapping to "${outputLigaturesJsonFilePath}" ...`);
-            await promisify(fs.writeFile)(outputLigaturesJsonFilePath, JSON.stringify(fontLigatures, null, 4));
+        let sourceScssFilePath = path.resolve(srcDirPath, 'material-design-icons.scss');
+        let outputCssFilePath = path.resolve(outputDirPath, 'material-design-icons.css');
 
-            let outputVariablesScssFilePath = path.resolve(srcDirPath, '_variables.scss');
-
-            console.log(`generating codepoints to "${outputVariablesScssFilePath}" ...`);
-            await writeVariablesSCSSFile(outputVariablesScssFilePath, fontLigatures);
-
-
-            let sourceScssFilePath = path.resolve(srcDirPath, 'material-design-icons.scss');
-            let outputCssFilePath = path.resolve(outputDirPath, 'material-design-icons.css');
-
-            console.log(`generating css file "${cssFilePath}" ...`);
-            await promisify(sass.render)({
-                file: sourceScssFilePath,
-                outFile: outputCssFilePath
-            });
-        }
-    }));
+        // console.log(`generating css file "${cssFilePath}" ...`);
+        await promisify(sass.render)({
+          file: sourceScssFilePath,
+          outFile: outputCssFilePath,
+        });
+      }
+    })
+  );
 }
 
-
 main()
-    .then(() => process.exit(0))
-    .catch(() => process.exit(1));
-
-
+  .then(() => process.exit(0))
+  .catch(() => process.exit(1));
